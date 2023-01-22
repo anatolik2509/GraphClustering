cfcb8a4d2e41f0628ede15f3ac4b69b165058e46
anatoly <antonov250901@mail.ru>
2023-01-22 20:17:00 +0300

    new algs

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import numpy as np
+
 import clustering
 import graph_generator
 import graph_utils
 import networkx as nx
+import kmedoids
 
 
 def mean_loss(losses: list):
@@ -19,17 +22,26 @@ if __name__ == '__main__':
         g = graph_generator.generate()
     g = graph_utils.add_random_weights_to_nodes(g, 50, 100)
     g = graph_utils.add_random_weights_to_edges(g, 50, 100)
-    partition_old_loses = []
-    partition_new_loses = []
-    for _ in range(1):
-        clusters_weights = [0.1, 0.4, 0.2, 0.3]
-        partitions_old = nx.community.asyn_fluidc(g, 4)
-        partitions_old = [part for part in partitions_old]
-        partition_old_loses.append(graph_utils.clustering_loss(g, partitions_old, clusters_weights))
-        graph_utils.draw_graph(g, partitions_old)
-        partitions_new = clustering.fluid_communities(g, clusters_weights)
-        partition_new_loses.append(graph_utils.clustering_loss(g, partitions_new, clusters_weights))
-        graph_utils.draw_graph(g, partitions_new)
-    print(mean_loss(partition_old_loses))
-    print(mean_loss(partition_new_loses))
+    partition_fluidc_loses = []
+    partition_new_fluidc_loses = []
+    partition_pam_loses = []
+    for _ in range(1000):
+        clusters_weights = [0.25, 0.25, 0.25, 0.25]
+        partitions_fluidc = nx.community.asyn_fluidc(g, 4)
+        partitions_fluidc = list(partitions_fluidc)
+        partition_fluidc_loses.append(graph_utils.clustering_loss(g, partitions_fluidc, clusters_weights))
+        # graph_utils.draw_graph(g, partitions_fluidc)
+        partitions_new_fluidc = clustering.fluid_communities(g, clusters_weights)
+        partition_new_fluidc_loses.append(graph_utils.clustering_loss(g, partitions_new_fluidc, clusters_weights))
+        # graph_utils.draw_graph(g, partitions_new_fluidc)
+        matrix = nx.to_numpy_matrix(g)
+        matrix[matrix == 0] = 10000
+        partitions_pam = [[] for _ in range(4)]
+        for i, l in enumerate(kmedoids.pam(matrix, 4).labels):
+            partitions_pam[l].append(i)
+        # graph_utils.draw_graph(g, partitions_pam)
+        partition_pam_loses.append(graph_utils.clustering_loss(g, partitions_pam, clusters_weights))
 
+    print(mean_loss(partition_fluidc_loses))
+    print(mean_loss(partition_new_fluidc_loses))
+    print(mean_loss(partition_pam_loses))
